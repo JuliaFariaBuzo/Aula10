08b3b6c4d3068eaf112531100862efba1c558b5e
Júlia <juliafariq@gmail.com>
2024-11-28 10:04:08 -0300

    28/11

--- servidor/index.js
+++ servidor/index.js
@@ -1,8 +1,8 @@
-const express= require('express');
-const cors = require('cors');
-const mysql = require('mysql2');
+const express = require ('express');
+const cors = require ('cors');
+const mysql = require ('mysql2');
 const app = express();
-const PORT =3001;
+const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
@@ -10,53 +10,47 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
+    user : 'root',
     password: 'mysqlfatec',
     database: 'aulabd'
-})
 
-//Conectando o banco de dados
+});
+
+//conectando ao banco de dados
 db.connect((erro) => {
-    if (erro){
-        console.error('Erro ao conectar ao MySQL:', erro);
-    }
-    else{
+    if (erro) {
+        console.erro('Erro ao conectar ao MySQL:'. erro);
+    } else {
         console.log('Conectado ao MySQL com sucesso!');
     }
 });
-
-//Rota para cadastrar usuário
-app.post('/alunos',(req,res)=>{
-    const {nome, cidade, estado} = req.body;
+//Rota para cadastrar usuario
+app.post('/alunos', (req, res) => {
+    const { nome, cidade, estado } = req.body;
 
     const sql = 'INSERT INTO alunos (nome, cidade, estado) VALUES (?, ?, ?)';
-
     db.query(sql, [nome, cidade, estado], (err, result) => {
-
-        if(err){
-            return res.status(500).json({ error: 'Erro ao cadastrar aluno!'});
-        }
-        res.status(201).json({message: 'Aluno cadastrado com sucesso!', id: result.insertId});
-
-    });
+    if (err) 
+    {
+        console.log(err);
+        return res.status(500).json({ error: 'Erro ao cadastrar aluno !'});
+    }
+    res.status(201).json({ message : 'Aluno cadastrado com sucesso!', id:result.insertId});
+    }); 
 });
 
-app.listen(PORT,()=>{
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
-
-//Rotac consulta aluno
 app.get('/alunos',(req,res)=>{
-    const {nome, cidade, estado} = req.body;
-
     const sql = 'SELECT * FROM alunos';
-
-    db.QUERY(sql, (err, results) => {
-
-        if(err){
-            return res.status(500).json({ error: 'Erro ao consultar aluno!'});
+    db.query(sql,(err,results)=>{
+        if (err) 
+        {
+            return res.status(500).json({ error: 'Erro ao consultar aluno !'});
         }
         res.json(results);
+    })
+});
 
-    });
+//Iniciando o servidor 
+app.listen(PORT, () =>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
